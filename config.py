36f0c7462c9c9bf74c9128f68c13747458acd253
Kate Engard <kateengard@Kates-MBP.PK5001Z>
2020-06-15 19:12:08 -0700

    fixed line 7

---
+++ config.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = "postgres://zwqrjawkrharue:b5e28e6d29cc4faa68ec9b34587e60bfbff74103430bed5b16f6da8c2656b57c@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d9d0jrbu108cjl"
--- pet_pals/static/js/app.js
+++ pet_pals/static/js/app.js
@@ -4,6 +4,7 @@ function buildPlot() {
 
     /* data route */
   const url = "/api/pals";
+  
   d3.json(url).then(function(response) {
 
     console.log(response);
